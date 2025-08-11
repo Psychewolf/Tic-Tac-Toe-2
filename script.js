7d94a72fef2c835391b8d171a58ad138d59f8eea
Kazi Jaber <kazi.a.j.jaber@gmail.com>
2025-08-12 02:47:08 +0600

    diagonal checks are working, but vertical-horizontal only working for full (bad for 3+)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const ROW = 3
-const COLUMN = 3
+const ROW = 4
+const COLUMN = 4
 
 function Gameboard() {
     let board = []
     for (let row = 1; row <= ROW; row++) {
         for (let column = 1; column <= COLUMN; column++){
-            board.push(Cell(0,row,column))
+            board.push(Cell(String(row)+column,row,column))
         }
     }
 
@@ -13,7 +13,87 @@ function Gameboard() {
         return board[(row-1)*COLUMN + (column-1)]
     }
 
-    return {board,findCell}
+    checkWin = () => {
+        let result = 'lose'
+        // row check
+        for (let row = 1; row <= ROW; row++) {
+            let checkList = new Set
+            for (let column = 1; column <= COLUMN; column++){
+                checkList.add(board[(row-1)*COLUMN + (column-1)].getValue())
+            }
+
+            if (checkList.size == 1) {win = true;break}
+        }
+
+        // column check
+        for (let column = 1; column <= COLUMN; column++) {
+            let checkList = new Set
+            for (let row = 1; row <= ROW; row++){
+                checkList.add(board[(row-1)*COLUMN + (column-1)].getValue())
+            }
+            console.log(checkList)
+            if (checkList.size == 1) {win = true;break}
+        }
+
+        // diagonal check > r
+        for(let cell = 0; cell < ROW*COLUMN;cell++){
+            let checkList = new Set
+            let x = cell
+            let col = -9999999
+            for(let i = 0; i < 3;i++){
+
+                if(x >= ROW*COLUMN) {
+                    checkList.add('-1')
+                }
+                else {
+                    let newcol = ((x+1) % COLUMN) === 0 ? COLUMN : (x+1) % COLUMN;
+                    if(newcol>col){
+                        checkList.add(board[x].getValue())
+                        x+=COLUMN+1
+                    }
+                    else{checkList.add('-1')}
+                    col = newcol
+                }
+
+            }
+            if (checkList.size == 1){
+                console.log(checkList)
+                result = 'win'
+                }
+        }
+        console.log('-------------')
+        // diagonal check > l
+        for(let cell = 0; cell < ROW*COLUMN;cell++){
+            let checkList = new Set
+            let x = cell
+            let col = 9999999
+            for(let i = 0; i < 3;i++){
+
+                if(x >= ROW*COLUMN) {checkList.add('-1')}
+                else {
+                    let newcol = ((x+1) % COLUMN) === 0 ? COLUMN : (x+1) % COLUMN;
+                    if(newcol<col){
+                        checkList.add(board[x].getValue())
+                        x+=COLUMN-1
+                    }
+                    else{checkList.add('-1')}
+                    col = newcol
+
+                }
+
+            }
+            if (checkList.size == 1){
+                console.log(checkList)
+                result = 'win'
+                }
+        }
+            
+
+
+
+}
+
+    return {board,findCell,checkWin}
 }
 
 function Cell(valuee,row,column){
@@ -24,6 +104,7 @@ function Cell(valuee,row,column){
     getColumn = () => column
     return {getValue,changeValue,getRow,getColumn}
 }
+
+
 gameboard = Gameboard()
-gameboard.findCell(2,2).changeValue(2)
-console.log(gameboard.findCell(2,2).getValue())
+gameboard.checkWin()
